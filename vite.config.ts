a659206d672b8507e11a7dfe24715351eb9d1eef
Jonathan Ratliff <jonathannoah@users.noreply.github.com>
2025-10-07 21:13:00 -0700

    Pass rather than call minify_sync

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,12 @@ import prebuildBookmarklet from "./prebuild-bookmarklet-plugin.ts";
 import { transpile } from "Typescript";
 import { minify_sync } from "terser";
 
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
     cloudflare(),
     react(),
     tsconfigPaths(),
-    prebuildBookmarklet("./src/bookmarklet.ts", transpile, minify_sync()),
+    prebuildBookmarklet("./src/bookmarklet.ts", transpile, minify_sync),
   ],
 });
